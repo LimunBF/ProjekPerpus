ee08e253b8fe735165ac4ab625a5585731d5859e
LimunBF <lintangmunoh@gmail.com>
2023-11-16 12:35:06 +0700

    Insert

--- src/database_buku/CreateBuku.java
+++ src/database_buku/CreateBuku.java
@@ -7,154 +7,28 @@ import java.sql.Statement;
 import java.util.Scanner;
 
 public class CreateBuku {
-
-    public static void createjudulbuku(Scanner scanner){
+    public static void CreateDataBuku(String judul, String pengarang, String penerbit, String tahunterbit) {
         try {
             Connection koneksi = BookDataConnector.getConnection();
-            Statement st = koneksi.createStatement();
-            System.out.println("Koneksi Berhasil.");
 
-            System.out.print("Masukkan Judul Buku: ");
-            String judulbuku = scanner.nextLine();
-
-            String insertQuery = "INSERT INTO data_buku (Judul_Buku) VALUES (?)";
+            String insertQuery = "INSERT INTO data_buku (Judul_Buku, Pengarang, Penerbit, Tahun_terbit) VALUES (?, ?, ?, ?)";
             PreparedStatement preparedStatement = koneksi.prepareStatement(insertQuery);
-            preparedStatement.setString(2, judulbuku);
+            preparedStatement.setString(1, judul);
+            preparedStatement.setString(2, pengarang);
+            preparedStatement.setString(3, penerbit);
+            preparedStatement.setString(4, tahunterbit);
+
             int rowCount = preparedStatement.executeUpdate();
             if (rowCount > 0) {
-                System.out.println("Data judul berhasil dimasukkan ke dalam data buku.");
+                System.out.println("Data anggota berhasil dimasukkan ke dalam database.");
             } else {
-                System.out.println("Data judul gagal dimasukkan ke dalam data buku.");
+                System.out.println("Data anggota gagal dimasukkan ke dalam database.");
             }
-            st.close();
+
             preparedStatement.close();
             koneksi.close();
-            System.out.println("\nKoneksi Ditutup.....");
-        }
-        catch (ClassNotFoundException | SQLException ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
-
-    public static void createpengarangtbuku(Scanner scanner) {
-        try {
-            Connection koneksi = BookDataConnector.getConnection();
-            Statement st = koneksi.createStatement();
-            System.out.println("Koneksi Berhasil.");
-
-            System.out.print("Masukkan Pengarang Buku: ");
-            String pengarangbuku = scanner.nextLine();
-
-            String insertQuery = "INSERT INTO data_buku (Pengarang) VALUES (?)";
-            PreparedStatement preparedStatement = koneksi.prepareStatement(insertQuery);
-            preparedStatement.setString(3, pengarangbuku);
-            int rowCount = preparedStatement.executeUpdate();
-            if (rowCount > 0) {
-                System.out.println("Data judul berhasil dimasukkan ke dalam data buku.");
-            } else {
-                System.out.println("Data judul gagal dimasukkan ke dalam data buku.");
-            }
-            st.close();
-            preparedStatement.close();
-            koneksi.close();
-            System.out.println("\nKoneksi Ditutup.....");
         } catch (ClassNotFoundException | SQLException ex) {
             System.out.println(ex.getMessage());
         }
     }
-
-        public static void createpenerbittbuku (Scanner scanner){
-            try {
-                Connection koneksi = BookDataConnector.getConnection();
-                Statement st = koneksi.createStatement();
-                System.out.println("Koneksi Berhasil.");
-
-                System.out.print("Masukkan Penerbit Buku: ");
-                String penerbitbuku = scanner.nextLine();
-
-                String insertQuery = "INSERT INTO data_buku (Penerbit) VALUES (?)";
-                PreparedStatement preparedStatement = koneksi.prepareStatement(insertQuery);
-                preparedStatement.setString(4, penerbitbuku);
-                int rowCount = preparedStatement.executeUpdate();
-                if (rowCount > 0) {
-                    System.out.println("Data judul berhasil dimasukkan ke dalam data buku.");
-                } else {
-                    System.out.println("Data judul gagal dimasukkan ke dalam data buku.");
-                }
-                st.close();
-                preparedStatement.close();
-                koneksi.close();
-                System.out.println("\nKoneksi Ditutup.....");
-            }
-            catch (ClassNotFoundException | SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
-        }
-
-        public static void createtahunterbitbuku (Scanner scanner){
-            try {
-                Connection koneksi = BookDataConnector.getConnection();
-                Statement st = koneksi.createStatement();
-                System.out.println("Koneksi Berhasil.");
-
-                System.out.print("Masukkan Tahun Terbit Buku: ");
-                String tahunterbit = scanner.nextLine();
-
-                String insertQuery = "INSERT INTO data_buku (Tahun_terbit) VALUES (?)";
-                PreparedStatement preparedStatement = koneksi.prepareStatement(insertQuery);
-                preparedStatement.setString(5, tahunterbit);
-                int rowCount = preparedStatement.executeUpdate();
-                if (rowCount > 0) {
-                    System.out.println("Data judul berhasil dimasukkan ke dalam data buku.");
-                } else {
-                    System.out.println("Data judul gagal dimasukkan ke dalam data buku.");
-                }
-                st.close();
-                preparedStatement.close();
-                koneksi.close();
-                System.out.println("\nKoneksi Ditutup.....");
-            }
-            catch (ClassNotFoundException | SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
-        }
-//    public static void createInputbuku(Scanner scanner) {
-//        try {
-//            Connection koneksi = BookDataConnector.getConnection();
-//            Statement st = koneksi.createStatement();
-//            System.out.println("Koneksi Berhasil");
-//
-//            System.out.print("Masukkan Nama Lengkap Anda: ");
-//            String judulbuku = scanner.nextLine();
-//
-//            System.out.print("Masukkan NIM Anda: ");
-//            String pengarang = scanner.nextLine();
-//
-//            System.out.print("Masukkan Program Pendidikan/Jurusan Anda: ");
-//            String penerbit = scanner.nextLine();
-//
-//            System.out.print("Masukkan Program Pendidikan/Jurusan Anda: ");
-//            String tahunterbit = scanner.nextLine();
-//
-//            String insertQuery = "INSERT INTO data_buku (Judul_Buku, Pengarang, Penerbit, Tahun_terbit) VALUES (?, ?, ?, ?)";
-//
-//            PreparedStatement preparedStatement = koneksi.prepareStatement(insertQuery);
-//            preparedStatement.setString(2, judulbuku);
-//            preparedStatement.setString(3, pengarang);
-//            preparedStatement.setString(4, penerbit);
-//            preparedStatement.setString(5, tahunterbit );
-//            int rowCount = preparedStatement.executeUpdate();
-//            if (rowCount > 0) {
-//                System.out.println("Data berhasil dimasukkan ke dalam data buku.");
-//            } else {
-//                System.out.println("Data gagal dimasukkan ke dalam data buku.");
-//            }
-//            st.close();
-//            preparedStatement.close();
-//            koneksi.close();
-//            System.out.println("\nKoneksi Ditutup.....");
-//        } catch (ClassNotFoundException | SQLException ex) {
-//            System.out.println(ex.getMessage());
-//        }
-//    }
 }
--- src/frame/insertBuku.java
+++ src/frame/insertBuku.java
@@ -29,37 +29,39 @@ public class insertBuku extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
-        jTextField3 = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
+        LabelJudul = new javax.swing.JLabel();
+        LabelPengarang = new javax.swing.JLabel();
+        LabelTahunterbit = new javax.swing.JLabel();
+        TextJudul = new javax.swing.JTextField();
+        TextPengarang = new javax.swing.JTextField();
+        TextTahunTerbit = new javax.swing.JTextField();
+        InsertButton = new javax.swing.JButton();
         Back = new javax.swing.JToggleButton();
+        LabelPenerbit = new javax.swing.JLabel();
+        TextPenerbit = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jLabel1.setFont(new java.awt.Font("Trebuchet MS", 1, 24)); // NOI18N
         jLabel1.setText("Input Buku");
 
-        jLabel2.setText("Judul Buku ");
+        LabelJudul.setText("Judul Buku ");
 
-        jLabel3.setText("Pengarang Buku");
+        LabelPengarang.setText("Pengarang Buku");
 
-        jLabel4.setText("Tahun Terbit");
+        LabelTahunterbit.setText("Tahun Terbit");
 
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+        TextJudul.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField1ActionPerformed(evt);
+                TextJudulActionPerformed(evt);
             }
         });
 
-        jButton1.setText("Insert");
-        jButton1.setActionCommand("");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        InsertButton.setText("Insert");
+        InsertButton.setActionCommand("");
+        InsertButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                InsertButtonActionPerformed(evt);
             }
         });
 
@@ -71,6 +73,8 @@ public class insertBuku extends javax.swing.JFrame {
             }
         });
 
+        LabelPenerbit.setText("Penerbit Buku");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -79,72 +83,83 @@ public class insertBuku extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(237, 237, 237)
-                        .addComponent(jLabel1))
+                        .addComponent(jLabel1)
+                        .addGap(0, 0, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(83, 83, 83)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 271, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                .addComponent(Back)
-                                .addGroup(layout.createSequentialGroup()
-                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, 102, Short.MAX_VALUE)
-                                        .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addGroup(layout.createSequentialGroup()
-                                            .addGap(61, 61, 61)
-                                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 271, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                            .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 271, javax.swing.GroupLayout.PREFERRED_SIZE))))))))
-                .addContainerGap(71, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(LabelJudul, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(LabelPengarang, javax.swing.GroupLayout.DEFAULT_SIZE, 102, Short.MAX_VALUE)
+                                    .addComponent(LabelTahunterbit, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(Back))
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addGap(61, 61, 61)
+                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                            .addComponent(TextJudul, javax.swing.GroupLayout.PREFERRED_SIZE, 271, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                            .addComponent(TextPengarang, javax.swing.GroupLayout.PREFERRED_SIZE, 271, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                            .addComponent(TextTahunTerbit, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 271, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                            .addComponent(InsertButton, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addComponent(LabelPenerbit)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(TextPenerbit, javax.swing.GroupLayout.PREFERRED_SIZE, 271, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(8, 8, 8)))))
                 .addGap(63, 63, 63))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(51, 51, 51)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(38, 38, 38)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(27, 27, 27)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel3)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(22, 22, 22)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel4)
-                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(46, 46, 46)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(Back))
-                .addContainerGap(62, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(Back))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(51, 51, 51)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(38, 38, 38)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(LabelJudul)
+                            .addComponent(TextJudul, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(27, 27, 27)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(LabelPengarang)
+                            .addComponent(TextPengarang, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(LabelPenerbit)
+                            .addComponent(TextPenerbit, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(LabelTahunterbit)
+                            .addComponent(TextTahunTerbit, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 48, Short.MAX_VALUE)
+                        .addComponent(InsertButton, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(30, 30, 30))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+    private void TextJudulActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TextJudulActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField1ActionPerformed
+    }//GEN-LAST:event_TextJudulActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void InsertButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_InsertButtonActionPerformed
         // TODO add your handling code here:
         try {
             // TODO add your handling code here:
-            String nama = TextNama.getText();
-            String nim = TextNIM.getText();
-            String fakultas = TextFakultas.getText();
-            String prodi = TextProdi.getText();
+            String judul = TextJudul.getText();
+            String pengarang = TextPengarang.getText();
+            String Penerbit = TextPenerbit.getText();
+            String tahunterbit = TextTahunTerbit.getText();
 
-            CreateAnggota.createDataAnggota(nama, nim, fakultas, prodi);
+           CreateBuku.CreateDataBuku(judul, pengarang, Penerbit, tahunterbit);
 
             DataAnggota framedata = new DataAnggota();
             framedata.setVisible(true);
@@ -152,7 +167,7 @@ public class insertBuku extends javax.swing.JFrame {
         }   catch (Exception e) {
             e.printStackTrace();
         } 
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_InsertButtonActionPerformed
 
     private void BackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BackActionPerformed
         // TODO add your handling code here:
@@ -197,13 +212,15 @@ public class insertBuku extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JToggleButton Back;
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton InsertButton;
+    private javax.swing.JLabel LabelJudul;
+    private javax.swing.JLabel LabelPenerbit;
+    private javax.swing.JLabel LabelPengarang;
+    private javax.swing.JLabel LabelTahunterbit;
+    private javax.swing.JTextField TextJudul;
+    private javax.swing.JTextField TextPenerbit;
+    private javax.swing.JTextField TextPengarang;
+    private javax.swing.JTextField TextTahunTerbit;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
     // End of variables declaration//GEN-END:variables
 }
